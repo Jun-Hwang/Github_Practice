9df282ef52506f1e31ebd9c970737e6fb1bcbbb5
Jun Hwang <hsj1901@gmail.com>
2022-09-28 13:28:28 -0400

    v0.1

---
+++ app.js
@@ -0,0 +1,10 @@
+const Jun = {
+    name: "Jun",
+    age: 32, 
+    height: "5,10",
+    ILoveYou:function(love){
+        console.log("I love you so much " + love);
+    }
+};
+
+Jun.ILoveYou("Kaitlyn");
