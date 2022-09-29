8d15bffb7697f3387fafad971416afe090b945d8
Jun Hwang <hsj1901@gmail.com>
2022-09-29 13:20:30 -0400

    v0.2 - calculator

--- app.js
+++ app.js
@@ -8,3 +8,31 @@ const Jun = {
 };
 
 Jun.ILoveYou("Kaitlyn");
+
+const calculator = {
+    add: function (a, b) {
+        console.log (a + b);
+    },
+
+    minus: function (a, b) {
+        console.log (a - b);
+    }, 
+
+    times: function (a, b) {
+        console.log (a * b);
+    },
+
+    devide: function(a, b) {
+        console.log (a / b);
+    },
+
+    power: function (a, b) {
+        console.log (a**b);
+    }
+};
+
+calculator.add(2,3);
+calculator.minus(2,3);
+calculator.times(2,3);
+calculator.devide(2,3);
+calculator.power(2,3);
